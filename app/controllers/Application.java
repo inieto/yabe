5138ecff9801cfffc5ddef9cb5f0a7c45849ea57
Ignacio Nieto <nietofarias@gmail.com>
2015-06-13 13:03:56 -0300

    guide 5 finished

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,6 +1,10 @@
 package controllers;
 
 import play.*;
+import play.cache.Cache;
+import play.data.validation.Required;
+import play.libs.Codec;
+import play.libs.Images;
 import play.mvc.*;
 
 import java.util.*;
@@ -20,5 +24,39 @@ public class Application extends Controller {
         List<Post> olderPosts = Post.find("order by postedAt desc").from(1).fetch(10);
         render(frontPost, olderPosts);
     }
+    
+    public static void show(Long id) {
+        Post post = Post.findById(id);
+        String randomID = Codec.UUID();
+        render(post, randomID);
+    }
 
+    /* Now we just have to validate the captcha. We have added the randomID as an hidden field right? 
+     * So we can retrieve it in the postComment action, then retrieve the actual code from Cache and 
+     * finally compare it to the submitted code.*/
+    public static void postComment(
+        Long postId, 
+        @Required(message="Author is required") String author, 
+        @Required(message="A message is required") String content, 
+        @Required(message="Please type the code") String code, 
+        String randomID)
+    {
+        Post post = Post.findById(postId);
+        validation.equals(code, Cache.get(randomID))
+            .message("Invalid code. Please type it again");
+        if (validation.hasErrors()) {
+            render("Application/show.html", post, randomID);
+        }
+        post.addComment(author, content);
+        flash.success("Thanks for posting %s", author);
+        Cache.delete(randomID);
+        show(postId);
+    }
+    
+    public static void captcha(String id) {
+        Images.Captcha captcha = Images.captcha();
+        String code = captcha.getText("#E4EAFD");
+        Cache.set(id, code, "10mn");
+        renderBinary(captcha);
+    }
 }
