b5a777dfe389beb85b5d916b04937c15e223037c
Ignacio Nieto <nietofarias@gmail.com>
2015-06-14 18:16:52 -0300

    guide 10 done

--- app/controllers/Posts.java
+++ app/controllers/Posts.java
@@ -2,7 +2,9 @@ package controllers;
  
 import play.*;
 import play.mvc.*;
- 
+
+@Check("admin")
+@With(Secure.class)
 public class Posts extends CRUD {
 	/* The convention is to pluralize the model object name to create the corresponding controller.
 	 * This way, Play will automatically find the associated model object for each controller. 
